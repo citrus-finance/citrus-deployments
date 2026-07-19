9694a19f74ffc2533faf3b018da2a2c42b11454c
0xtomm <84812080+0xtomm@users.noreply.github.com>
2026-07-19 17:28:36 +0100

    chore(tokens): update VotesToken

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import { writeJSONOutput, writeOutput } from "./lib/output.ts";
 const { getContractReader } = await foundryBuild(
   "git@github.com:citrus-finance/citrus-tokens.git",
   {
-    commit: "76a2c80b25983b761d886526f40ef447fdc323ca",
+    commit: "95abf07d9c0432ce673ea5099c38d7c6ce67459c",
   },
 );
 
 const ERC20VotesContract = getContractReader("VotesToken");
 
-await writeJSONOutput("contracts/VotesToken/abi.json", ERC20VotesContract.getABI());
-await writeOutput("contracts/VotesToken/bytecode", ERC20VotesContract.getBytecode());
+await writeJSONOutput("contracts/tokens/VotesToken/abi.json", ERC20VotesContract.getABI());
+await writeOutput("contracts/tokens/VotesToken/bytecode", ERC20VotesContract.getBytecode());
